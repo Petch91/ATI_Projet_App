dd39155fc014d655e30fd33f0f9b13c78a66408f
Pierre PIRNAY <Pierre.PIRNAY@eiffage.com>
2024-04-26 12:04:16 +0200

    test3001

--- ATI_Projet_Components/Emails/EmailForm.razor.cs
+++ ATI_Projet_Components/Emails/EmailForm.razor.cs
@@ -2,8 +2,6 @@ using ATI_Projet_Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System.Reflection;
-using System.Text;
-using System.Web;
 
 namespace ATI_Projet_Components.Emails
 {
@@ -16,8 +14,7 @@ namespace ATI_Projet_Components.Emails
         [Parameter] public string Error { get; set; }
 
         private List<string> descriptions = new List<string> { "Privée", "Professionnelle" };
-        private List<byte[]> descriptionsByte = new List<byte[]> { Encoding.UTF8.GetBytes("Privée"), Encoding.UTF8.GetBytes("Professionnelle") };
-
+       
         public void IsValided()
         {
                 OnValidation.InvokeAsync(Email);               
