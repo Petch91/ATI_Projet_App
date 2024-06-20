e6ea9921b2d3d5ef9d7f651227639d85018307f0
Pierre PIRNAY <Pierre.PIRNAY@eiffage.com>
2024-06-20 14:56:06 +0200

    test

--- ATI_Projet_App/Tools/AuthStateProvider.cs
+++ ATI_Projet_App/Tools/AuthStateProvider.cs
@@ -7,52 +7,54 @@ using System.Security.Cryptography;
 
 namespace ATI_Projet_App.Tools
 {
-    public class AuthStateProvider(ProtectedLocalStorage storage) : AuthenticationStateProvider
-    {
-        private readonly ProtectedLocalStorage _storage = storage;
-        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
-        {
-            List<Claim> claims = new List<Claim>();
-            ProtectedBrowserStorageResult<string> result = new ProtectedBrowserStorageResult<string>();
-            try
-            {
-                result = await _storage.GetAsync<string>("Token");
-            }
-            catch (CryptographicException e)
-            {
-                await _storage.DeleteAsync("Token");
-                await _storage.DeleteAsync("ConnectedUser");
-                result = await _storage.GetAsync<string>("Token");
-            }
-            string token = result.Value;
+   public class AuthStateProvider(ProtectedLocalStorage storage, NavigationManager navigationManager) : AuthenticationStateProvider
+   {
+      private readonly ProtectedLocalStorage _storage = storage;
+      private readonly NavigationManager _navigationManager = navigationManager;
+      public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+      {
+         List<Claim> claims = new List<Claim>();
+         ProtectedBrowserStorageResult<string> result = new ProtectedBrowserStorageResult<string>();
+         try
+         {
+            result = await _storage.GetAsync<string>("Token");
+         }
+         catch (CryptographicException e)
+         {
+            //await _storage.DeleteAsync("Token");
+            //await _storage.DeleteAsync("ConnectedUser");
+            _navigationManager.Refresh(true);
+            result = await _storage.GetAsync<string>("Token");
+         }
+         string token = result.Value;
 
-            if (string.IsNullOrWhiteSpace(token))
-            {
-                ClaimsIdentity anonymousUser = new ClaimsIdentity();
-                return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymousUser)));
-            }
+         if (string.IsNullOrWhiteSpace(token))
+         {
+            ClaimsIdentity anonymousUser = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymousUser)));
+         }
 
-            JwtSecurityToken jwt = new JwtSecurityToken(token);
-            if (jwt.ValidTo < DateTime.UtcNow)
-            {
-                await _storage.DeleteAsync("Token");
-                await _storage.DeleteAsync("ConnectedUser");
-                ClaimsIdentity anonymousUser = new ClaimsIdentity();
-                return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymousUser)));
-            }
-            foreach (Claim claim in jwt.Claims)
-            {
-                claims.Add(claim);
-            }
+         JwtSecurityToken jwt = new JwtSecurityToken(token);
+         if (jwt.ValidTo < DateTime.UtcNow)
+         {
+            await _storage.DeleteAsync("Token");
+            await _storage.DeleteAsync("ConnectedUser");
+            ClaimsIdentity anonymousUser = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymousUser)));
+         }
+         foreach (Claim claim in jwt.Claims)
+         {
+            claims.Add(claim);
+         }
 
-            ClaimsIdentity currentUser = new ClaimsIdentity(claims, "TestAuthSystem");
+         ClaimsIdentity currentUser = new ClaimsIdentity(claims, "TestAuthSystem");
 
 
-            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal(currentUser))).Result;
-        }
-        public void NotifyUserChanged()
-        {
-            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
-        }
-    }
+         return Task.FromResult(new AuthenticationState(new ClaimsPrincipal(currentUser))).Result;
+      }
+      public void NotifyUserChanged()
+      {
+         NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+      }
+   }
 }
