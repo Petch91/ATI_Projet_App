ce4813efb223ed1f008b66e4b8bf28157781a566
Pierre PIRNAY <Pierre.PIRNAY@eiffage.com>
2024-06-26 10:47:55 +0200

    test login

---
+++ ATI_Projet_App/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using ATI_Projet_App.Tools;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph.Models.CallRecords;
+
+namespace ATI_Projet_App.Controllers
+{
+   [Route("[controller]/[action]")]
+   [ApiController]
+   public class LoginController(SessionManager sessionManager) : ControllerBase
+   {
+      private readonly SessionManager _sessionManager = sessionManager;
+
+      [HttpPost]
+      public IActionResult Login([FromBody] LoginExt login)
+      {
+         if (_sessionManager.Login(login.Id, login.HashToken).Result) return Ok();
+         else return BadRequest();
+      }
+   }
+   public class LoginExt
+   {
+      public int Id { get; set; }
+      public string HashToken { get; set; }
+   }
+
+}
--- ATI_Projet_App/Tools/SessionManager.cs
+++ ATI_Projet_App/Tools/SessionManager.cs
@@ -1,67 +1,91 @@
-﻿using ATI_Projet_App.Models;
+﻿using ATI_Projet_App.Controllers;
+using ATI_Projet_App.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace ATI_Projet_App.Tools
 {
-    public class SessionManager(ProtectedLocalStorage protectedLocalStorage )
-    {
-        private readonly ProtectedLocalStorage _protectedLocalStorage = protectedLocalStorage;
+   public class SessionManager(ProtectedLocalStorage protectedLocalStorage, HttpClient httpClient, NavigationManager navigationManager)
+   {
+      private readonly ProtectedLocalStorage _protectedLocalStorage = protectedLocalStorage;
+      private readonly HttpClient _client = httpClient;
+      private readonly NavigationManager _navigationManager = navigationManager;
 
+      public User? ConnectedUser
+      {
+         //get
+         //{
+         //    return Get<User>("ConnectedUser").Result;                   
+         //}
+         set
+         {
+            Set<User>("ConnectedUser", value);
+         }
+      }
 
-        public User? ConnectedUser
-        {
-            //get
-            //{
-            //    return Get<User>("ConnectedUser").Result;                   
-            //}
-            set
+      public string Token
+      {
+         //get
+         //{
+         //    return Get<string>("Token").Result;
+         //}
+         set
+         {
+            Set<string>("Token", value);
+         }
+      }
+
+      //public async Task<T> Get<T>(string key)
+      //{
+      //    var result = await _protectedLocalStorage.GetAsync<T>(key);
+      //    return result.Value;
+      //}
+
+      private async Task Set<T>(string key, T value)
+      {
+         await _protectedLocalStorage.SetAsync(key, value);
+      }
+
+      public async Task<User> GetUser()
+      {
+         var result = await _protectedLocalStorage.GetAsync<User>("ConnectedUser");
+         return result.Value;
+      }
+
+      public async Task<string> GetToken()
+      {
+         var result = await _protectedLocalStorage.GetAsync<string>("Token");
+         return result.Value;
+      }
+
+      public async Task Logout()
+      {
+         await _protectedLocalStorage.DeleteAsync("ConnectedUser");
+         await _protectedLocalStorage.DeleteAsync("Token");
+
+      }
+      public async Task<bool> Login(int Id, string HashToken)
+      {
+
+         //_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Value);
+         using (HttpResponseMessage response = await _client.PostAsJsonAsync<LoginExt>($"http://192.168.123.69:7000/api/user/token/", new LoginExt { Id = Id, HashToken = HashToken }))
+         {
+            if (response.IsSuccessStatusCode)
             {
-                Set<User> ("ConnectedUser", value);
+               return true;
             }
-        }
-
-        public string Token
-        {
-            //get
-            //{
-            //    return Get<string>("Token").Result;
-            //}
-            set
+            else
             {
-                Set<string>("Token", value);
+               return false;
             }
-        }
 
-        //public async Task<T> Get<T>(string key)
-        //{
-        //    var result = await _protectedLocalStorage.GetAsync<T>(key);
-        //    return result.Value;
-        //}
-
-        private async Task Set<T>(string key , T value)
-        {
-            await _protectedLocalStorage.SetAsync(key, value);
-        }
-
-        public async Task<User> GetUser()
-        {
-            var result = await _protectedLocalStorage.GetAsync<User>("ConnectedUser");
-            return result.Value;
-        }
-
-        public async Task<string> GetToken()
-        {
-            var result = await _protectedLocalStorage.GetAsync<string>("Token");
-            return result.Value;
-        }
-
-        public async Task Logout()
-        {
-            await _protectedLocalStorage.DeleteAsync("ConnectedUser");
-            await _protectedLocalStorage.DeleteAsync("Token");
-            
-        }
-    }
+         }
+      }
+   }
 }
+   
